95a4b68d526ff4c2682c6e53ce0d319542d9b1d7
Victor Aluko <victor.aluko@gmail.com>
2014-08-09 11:43:53 +0100

    Created the schema for the products DB

---
+++ sql_schemas/ek_products_backup.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.24-log : Database - ek_products
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ek_products` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `ek_products`;
+
+/*Table structure for table `product_items` */
+
+DROP TABLE IF EXISTS `product_items`;
+
+CREATE TABLE `product_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `sku_id` varchar(100) NOT NULL,
+  `status` int(11) DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `product_items` */
+
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `short_description` tinytext NOT NULL,
+  `description` text NOT NULL,
+  `price` double NOT NULL,
+  `stock` int(11) NOT NULL DEFAULT '0',
+  `image_path` varchar(200) DEFAULT NULL,
+  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`,`created_on`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `products` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
